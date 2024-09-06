8d2a5de358abc71eed3731b5be7a7cb88662d522
muchluvu <rorrxr@naver.com>
2024-09-07 00:13:15 +0900

    first commit

---
+++ src/main/java/com/etergg/RepoBisApplication.java
@@ -0,0 +1,13 @@
+package com.etergg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoBisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoBisApplication.class, args);
+	}
+
+}
