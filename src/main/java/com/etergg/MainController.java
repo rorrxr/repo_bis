5784a108e7aeb9a58df94a0d93653a7da3adb73c
muchluvu <rorrxr@naver.com>
2024-11-04 18:03:14 +0900

    타임리프 href 수정

--- src/main/java/com/etergg/MainController.java
+++ src/main/java/com/etergg/MainController.java
@@ -1,13 +1,32 @@
 package com.etergg;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+    @Autowired
+    MainService mainService;
+
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+    @GetMapping("/projects")
+    public String projects(){
+        return "projects";
+    }
+
+    @GetMapping("/resume")
+    public String resume(){
+        return "resume";
     }
 }
