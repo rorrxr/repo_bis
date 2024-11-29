7dbcce4a545259e275c2ce0d278702707953625a
muchluvu <rorrxr@naver.com>
2024-11-29 22:36:30 +0900

    gitgnore 수정

--- src/main/java/com/etergg/repository/UserJoinRepository.java
+++
@@ -1,9 +0,0 @@
-package com.etergg.repository;
-
-import com.etergg.entity.UserJoin;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserJoinRepository extends JpaRepository<UserJoin, Long> {
-}
